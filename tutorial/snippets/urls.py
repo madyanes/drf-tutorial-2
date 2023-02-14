08efdad9359f20246826b1a29759d013225c5996
madyanes <madyanes.1995@gmail.com>
2023-02-14 13:19:21 +0700

    using mixins class: snippet list

--- tutorial/snippets/urls.py
+++ tutorial/snippets/urls.py
@@ -3,7 +3,7 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from snippets import views
 
 urlpatterns = [
-    path('', views.SnippetList.as_view()),
+    path('', views.SnippetListWithMixin.as_view()),
     path('<int:pk>/', views.SnippetDetail.as_view()),
 ]
 
--- tutorial/snippets/views.py
+++ tutorial/snippets/views.py
@@ -1,10 +1,22 @@
 from django.http import Http404
-from rest_framework import status
+from rest_framework import generics, mixins, status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from snippets.models import Snippet
 from snippets.serializers import SnippetModelSerializer
+
+class SnippetListWithMixin(mixins.ListModelMixin,
+                           mixins.CreateModelMixin,
+                           generics.GenericAPIView):
+    queryset = Snippet.objects.all()
+    serializer_class = SnippetModelSerializer
+
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
 
 class SnippetList(APIView):
     """
